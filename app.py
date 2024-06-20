dc45cc292e577a22bba5668283520fe5ee451db1
Dmitry Ryzhenkov <cuicui@arch>
2024-06-20 18:34:59 +0200

    env

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 from dotenv import load_dotenv
+import os
 
 def main():
     st.title("Environment variables")
 
+    try:
+        load_dotenv()
+    except: pass
+
+    at = os.environ["AIRTABLE_TOKEN"]
+    ow = os.environ["OPENWEATHER_KEY"]
+
+    st.success(at)
+    st.success(ow)
 
 if __name__ == "__main__":
     main()
